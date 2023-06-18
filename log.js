141153017b1a706ab8927911f14e608bcbfce5a1
Abdulahi143 <qaskaxaji143@gmail.com>
2023-06-18 17:12:06 +0200

    validation done

--- log.js
+++ log.js
@@ -2,48 +2,51 @@ const username = document.querySelector('#username');
 const password = document.querySelector('#password');
 const btnLogin = document.querySelector('.btn-login');
 
-const getUsersFromLocal = function(){
+const getUsersFromLocal = function() {
     let users = localStorage.getItem('users');
     return users ? JSON.parse(users) : [];
 }
 
-const getUserInfoFromLocal = function(){
+const getUserInfoFromLocal = function() {
     let userInformation = localStorage.getItem('userInformation');
     return userInformation ? JSON.parse(userInformation) : null;
 }
 
 const userInformations = getUserInfoFromLocal();
 
-if(userInformations){
+if (userInformations) {
     window.location.href = '/';
 }
 
-btnLogin.addEventListener('click', function(event){
+btnLogin.addEventListener('click', function(event) {
     event.preventDefault();
-
-    if(username.value.length > 0 && password.value.length > 0 && username.value !== null && password.value !== null){
-        const user = getUsersFromLocal().find(function(user){
+ 
+    if (validateEmail(username.value) && password.value.length >= 8) {
+        const user = getUsersFromLocal().find(function(user) {
             return user.email === username.value && user.password === password.value;
         });
-        
-        if(user){
+
+        if (user) {
             const userInformation = {
                 username: user.email,
                 lastName: user.fullName.split(' ')[1]
             };
-        
+
             localStorage.setItem('userInformation', JSON.stringify(userInformation));
             window.location.href = '/';
         } else {
-            console.log('Login failed. Invalid username or password.');
+            alert('User not registered. Please register before logging in.');
         }
     } else {
-        console.log('Invalid username or password.');
+        showAlert('Invalid username or password.');
     }
 });
 
+function validateEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
 
-/*The getUserInfoFromLocal function is corrected to properly parse the userInformation data from localStorage.
-The condition for checking if the username and password are valid is modified to use the logical && operator instead of ||.
-The condition for checking if the user is found is corrected to check if the user object exists.
-The error message for failed login attempts is updated to be more descriptive.*/
+function showAlert(message) {
+    alert(message);
+}
--- reg.js
+++ reg.js
@@ -1,97 +1,94 @@
 const logout = document.querySelector('.logout');
-const lastName = document.querySelector('.lastname')
-const imageUrl = document.querySelector('#imageUrl')
-const title = document.querySelector('#title')
-const article = document.querySelector('#article')
+const lastName = document.querySelector('.lastname');
+const imageUrl = document.querySelector('#imageUrl');
+const title = document.querySelector('#title');
+const article = document.querySelector('#article');
 const btnPost = document.querySelector('.btn-Post');
 const postDisplay = document.querySelector('.posts');
 
-// get user information
-const getUserInfoFromLocal = function(){
-    let users = localStorage.getItem('userInformation');
-    return users ? JSON.parse(users) : [];
-}
-
-
+// Get user information
+const getUserInfoFromLocal = function() {
+  let users = localStorage.getItem('userInformation');
+  return users ? JSON.parse(users) : [];
+};
 
 const userInfo = getUserInfoFromLocal();
 
-if(!userInfo){
-    window.location.href = '/log.html'
+if (!userInfo) {
+  window.location.href = '/log.html';
 }
 
-lastName.textContent = userInfo.lastName;
+// Display "Hello" before the last name
+lastName.textContent = 'Hello, ' + userInfo.lastName + '!';
 
-logout.addEventListener('click',function(){
-    window.location.href = '/log.html'
-    localStorage.setItem('userInformation', JSON.stringify(''));
+logout.addEventListener('click', function() {
+  window.location.href = '/log.html';
+  localStorage.setItem('userInformation', JSON.stringify(''));
 });
 
-const getPosts = function(){
-    let posts = localStorage.getItem('posts');
-    return posts ? JSON.parse(posts) : [];
-}
+const getPosts = function() {
+  let posts = localStorage.getItem('posts');
+  return posts ? JSON.parse(posts) : [];
+};
 
-const articleFun = function(posts){
-    const post = getPosts();
-    post.push(posts);
-    localStorage.setItem('posts', JSON.stringify(post));
-}
+const articleFun = function(posts) {
+  const post = getPosts();
+  post.push(posts);
+  localStorage.setItem('posts', JSON.stringify(post));
+};
 
-//Delete Post from localStorage
-const deletePost = function(postIndex){
-    const posts = getPosts();
-  const del =   posts.filter(function(post, index){
-        
-         return index !== postIndex;
-         
-    });
-    
-    
+// Delete Post from localStorage
+const deletePost = function(postIndex) {
+  const posts = getPosts();
+  const del = posts.filter(function(post, index) {
+    return index !== postIndex;
+  });
 
-   localStorage.setItem('posts', JSON.stringify(del));
-    displayArticles();
-}
+  localStorage.setItem('posts', JSON.stringify(del));
+  displayArticles();
+};
 
-btnPost.addEventListener('click',function(e){
-    e.preventDefault();
-    let lastArr = getPosts().length -1;
-    lastArr = lastArr + 1;
- 
-    const aricles = {
-        id : lastArr,
-        image : imageUrl.value,
-        title : title.value,
-        article: article.value,
-        postBy : userInfo.username
-    }
-    
-    articleFun(aricles);
-    alert('Added New Post');
+btnPost.addEventListener('click', function(e) {
+  e.preventDefault();
+  let lastArr = getPosts().length - 1;
+  lastArr = lastArr + 1;
 
-    
-    imageUrl.value = '';
-    title.value = '';
-    article.value = '';
-    displayArticles();
+  const articles = {
+    id: lastArr,
+    image: imageUrl.value,
+    title: title.value,
+    article: article.value,
+    postBy: userInfo.username
+  };
+
+  articleFun(articles);
+  alert('Added New Post');
+
+  imageUrl.value = '';
+  title.value = '';
+  article.value = '';
+  displayArticles();
 });
 
-const displayArticles = function(){
-    const posts = getPosts();
-    const userInformation = getUserInfoFromLocal();
-   
-     postDisplay.innerHTML = '';
-    posts.forEach((post, index) => {
-        postDisplay.innerHTML += `
-            <div class="post">
-                <img src="${post.image}" alt="invalid image" srcset="">
-                <h2>${post.title}</h2>
-                <p>${post.article}</p>
-                 
-                 ${userInformation.username === post.postBy ? `<button class="deleteButton" onclick="deletePost(${index})">Delete</button>`: ``}
-            </div>
-        `;
-    });
-}
+const displayArticles = function() {
+  const posts = getPosts();
+  const userInformation = getUserInfoFromLocal();
+
+  postDisplay.innerHTML = '';
+  posts.forEach((post, index) => {
+    postDisplay.innerHTML += `
+      <div class="post">
+        <img src="${post.image}" alt="invalid image" srcset="">
+        <h2>${post.title}</h2>
+        <p>${post.article}</p>
+        ${
+          userInformation.username === post.postBy
+            ? `<button class="deleteButton" onclick="deletePost(${index})">Delete</button>`
+            : ''
+        }
+      </div>
+    `;
+  });
+};
 
 displayArticles();
